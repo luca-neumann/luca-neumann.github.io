c0efd6b5ef7498f9c0e6c5d8a786ccaecb691eaa
LucaNeumann <ln@lucaneumann.com>
2025-04-13 23:28:51 +0200

    version 1.0.9

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template >
     <footer class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-        <div class="container p-12 flex justify-between">
+        <div class="container p-10 flex justify-between">
             <div class="text-white text-3xl font-bold">
                 <img src="@/assets/LN-Profile-white.png" alt="LN" width="50px" height="50px">
             </div>
-            <p class="text-slate-600">© All rights reserved.</p>
+            <p class="text-slate-600 text-center">© All rights reserved.</p>
         </div>
     </footer>
 </template>
